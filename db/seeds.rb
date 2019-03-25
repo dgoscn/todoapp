df27a47e37d7c552a02050f798c7acedaf6a670f
Diego Amaral <diego-ii@live.com>
2019-03-25 17:50:51 -0300

    added the files to a new file with ajax features

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Task.create! name: "Realizar migração do Poliedro", complete: true
+Task.create! name: "Pagamento dos funcionários", complete: true
+Task.create! name: "Checar base de testes"
+Task.create! name: "Conferir servidor na Amazon"
